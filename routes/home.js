d01593bbf349a8ecd30ea739b71a52917a48cea5
DipakGupta2006 <idipakgupta2006@gmail.com>
2026-06-20 22:33:31 +0530

    home page completed user

--- routes/home.js
+++ routes/home.js
@@ -3,16 +3,21 @@ const router = express.Router();
 const pool = require("../db");
 const isAuthenticated = require("../middleware/auth");
 
-router.get("/home", isAuthenticated , async(req,res)=>{
-    try{
-        const [rows] = await pool.query("SELECT * FROM banners LIMIT 1");
-        res.render("home",{banner: rows[0]});
+router.get("/home", isAuthenticated, async (req, res) => {
+    try {
+        // Banner fetch
+        const [bannerRows] = await pool.query("SELECT * FROM banners LIMIT 1");
+        // Latest Collection products fetch
+        const [products] = await pool.query("SELECT * FROM latest_collection WHERE status=true");
+        res.render("home", {
+            banner: bannerRows[0],
+            products: products
+        });
     }
-    catch(error){
+    catch (error) {
         console.log(error);
         res.send("Server Error");
     }
 });
-
 
 module.exports = router;
