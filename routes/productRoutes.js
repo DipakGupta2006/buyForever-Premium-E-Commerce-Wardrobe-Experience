90361746711eec4832a25c336c1ec9804935fb85
DipakGupta2006 <idipakgupta2006@gmail.com>
2026-06-22 16:15:28 +0530

    home page product id page done user

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,48 +1,25 @@
 const express = require("express");
 const router = express.Router();
-
 const pool = require("../db");
-
-
 // Product Details Page
 
 router.get("/product/:id", async (req, res) => {
-
     try {
-
         const productId = req.params.id;
-
-
         const [rows] = await pool.query(
             "SELECT * FROM latest_collection WHERE id = ?",
             [productId]
         );
-
-
         if (rows.length === 0) {
-
             return res.send("Product not found");
-
         }
-
-
         res.render("product-details", {
-
             product: rows[0]
-
         });
-
-
     } 
     catch(error) {
-
         console.log(error);
-
         res.send("Server Error");
-
     }
-
 });
-
-
 module.exports = router;    
